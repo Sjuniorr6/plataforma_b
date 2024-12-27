18dae161eac6f78a87e5c857d38c843f46f22ad9
Eurico Dante <eurico.dante@goldensat.local>
2024-12-27 17:22:36 -0300

    Arrumando .env

--- plataforma/plataforma/settings.py
+++ plataforma/plataforma/settings.py
@@ -10,7 +10,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env("SECRET_KEY")
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost"])
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[".railway.app", "localhost"])
 
 # Application definition
 INSTALLED_APPS = [
